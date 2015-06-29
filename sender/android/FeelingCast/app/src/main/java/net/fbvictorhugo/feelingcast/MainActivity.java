0b49f7a4a86252784f06e18c57e317ae85f4b685
Victor Hugo <fb.victorhugo@gmail.com>
2015-06-29 19:37:25 -0300

    Implementacoes iniciais para encontrar o dispositivo de cast

--- sender/android/FeelingCast/app/src/main/java/net/fbvictorhugo/feelingcast/MainActivity.java
+++ sender/android/FeelingCast/app/src/main/java/net/fbvictorhugo/feelingcast/MainActivity.java
@@ -1,23 +1,60 @@
 package net.fbvictorhugo.feelingcast;
 
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.MediaRouteActionProvider;
+import android.support.v7.media.MediaRouteSelector;
+import android.support.v7.media.MediaRouter;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.cast.CastDevice;
+import com.google.android.gms.cast.CastMediaControlIntent;
+
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "> GDG >>>";
+
+    private MediaRouter mMediaRouter;
+    private MediaRouteSelector mMediaRouteSelector;
+    private MediaRouter.Callback mMediaRouterCallback;
+    private CastDevice mCastDevice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMediaRouter = MediaRouter.getInstance(getApplicationContext());
+        mMediaRouteSelector = new MediaRouteSelector.Builder().addControlCategory(CastMediaControlIntent.categoryForCast(getResources().getString(R.string.cast_app_id))).build();
+        mMediaRouterCallback = new MyMediaRouterCallback();
     }
 
-    // region OPTIONS MENU
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mMediaRouter.addCallback(mMediaRouteSelector, mMediaRouterCallback,
+                MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY);
+    }
+
+    @Override
+    protected void onStop() {
+        mMediaRouter.removeCallback(mMediaRouterCallback);
+        super.onStop();
+    }
+
+    // region OptionsMenu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem mediaRouteMenuItem = menu.findItem(R.id.media_route_menu_item);
+        MediaRouteActionProvider mediaRouteActionProvider =
+                (MediaRouteActionProvider) MenuItemCompat.getActionProvider(mediaRouteMenuItem);
+        mediaRouteActionProvider.setRouteSelector(mMediaRouteSelector);
+
         return true;
     }
 
@@ -36,5 +73,24 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
     // endregion
-    
+
+    // region My MediaRouter.Callback
+    class MyMediaRouterCallback extends MediaRouter.Callback {
+
+        @Override
+        public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo route) {
+            //  super.onRouteSelected(router, route);
+            // Handle the user route selection.
+            mCastDevice = CastDevice.getFromBundle(route.getExtras());
+
+            //launchReceiver();
+        }
+
+        @Override
+        public void onRouteAdded(MediaRouter router, MediaRouter.RouteInfo route) {
+            // super.onRouteAdded(router, route);
+            Log.d(TAG, route.getName());
+        }
+    }
+    //endregion
 }
