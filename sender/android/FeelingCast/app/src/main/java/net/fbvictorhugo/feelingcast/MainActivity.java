110941e10ef9737d568f72ad3ffadd3c1220ef5f
Victor Hugo <fb.victorhugo@gmail.com>
2015-06-29 20:28:13 -0300

    Aplicando thema e cores ao app

--- sender/android/FeelingCast/app/src/main/java/net/fbvictorhugo/feelingcast/MainActivity.java
+++ sender/android/FeelingCast/app/src/main/java/net/fbvictorhugo/feelingcast/MainActivity.java
@@ -2,15 +2,19 @@ package net.fbvictorhugo.feelingcast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
     }
 
     // region OPTIONS MENU
@@ -36,5 +40,5 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
     // endregion
-    
+
 }
